9c513f3d73abf98c50f02d4a9a826c659d270669
LeonardoTeixeira23 <leo.dst23@gmail.com>
2023-07-10 20:27:37 -0300

    .

--- client.py
+++ client.py
@@ -13,40 +13,51 @@ def exibir_opcoes_votacao():
     print('2. Votar contra a inelegibilidade do ex-presidente')
 
 
-def votar(escolha):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-        client_socket.connect((ServerConfig.HOST, ServerConfig.PORTA))
-        client_socket.sendall(escolha.encode())
-
-        votacao_encerrada = client_socket.recv(1024).decode()
-
-        if votacao_encerrada == '0':
-            exibir_opcoes_votacao()
-            opcao = input('Digite o número da opção desejada: ')
-            client_socket.sendall(opcao.encode())
-
-            resposta = client_socket.recv(1024).decode()
-            print(resposta)
-        else:
-            resposta = client_socket.recv(1024).decode()
-            print(resposta)
-
-
-def receber_atualizacoes(escolha):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-        client_socket.connect((ServerConfig.HOST, ServerConfig.PORTA))
-        client_socket.sendall(escolha.encode())
-        while True:
-            resposta = client_socket.recv(1024).decode()
-            print(resposta)
-            if resposta.startswith('Votação encerrada:'):
-                break
-
-
 def iniciar_thread_acompanhamento(escolha):
     thread = threading.Thread(target=receber_atualizacoes, args=(escolha,))
     thread.start()
     thread.join()  # Aguarda até que a thread de atualizações seja concluída
+
+
+def conectar_servidor(funcao):
+    def funcao_interna(escolha):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+            client_socket.connect((ServerConfig.HOST, ServerConfig.PORTA))
+            client_socket.sendall(escolha.encode())
+            return funcao(client_socket)
+
+    return funcao_interna
+
+
+@conectar_servidor
+def votar(client_socket):
+    votacao_encerrada = client_socket.recv(1024).decode()
+
+    if votacao_encerrada == '0':
+        exibir_opcoes_votacao()
+        opcao = input('Digite o número da opção desejada: ')
+        client_socket.sendall(opcao.encode())
+
+        resposta = client_socket.recv(1024).decode()
+        print(resposta)
+        return msg_final(resposta)
+    else:
+        resposta = client_socket.recv(1024).decode()
+        print(resposta)
+        return msg_final(resposta)
+
+
+@conectar_servidor
+def receber_atualizacoes(client_socket):
+    while True:
+        resposta = client_socket.recv(1024).decode()
+        print(resposta)
+        if msg_final(resposta):
+            break
+
+
+def msg_final(msg):
+    return msg.startswith('Votação encerrada:')
 
 
 def executar_cliente():
@@ -57,10 +68,14 @@ def executar_cliente():
 
         escolha = input('Escolha uma opção: ')
         if escolha == '1':
-            votar(escolha)
+            votacao_encerrada = votar(escolha)  # Sem passar a escolha como argumento
+            if votacao_encerrada:
+                break
+
         elif escolha == '2':
             iniciar_thread_acompanhamento(escolha)
             break
+
         else:
             print('Opção inválida!')
 
